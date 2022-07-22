92b8246b71213b0d0c51791125ad2d22a14fe3a3
lacha.arg@gmail.com <lacha.arg@gmail.com>
2022-07-21 23:59:09 -0300

    Project almost finished (this is not a great title)
    HTML and css is done. I still need to make it responsive for mobile. Also, need to understand why i couldn't add/remove classes on buttons from a nodelist with an event listener.

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 "strict mode";
 const container = document.querySelector(".container");
 const btns = document.querySelectorAll(".grid-size");
-const blackBtn = document.getElementById("blackBtn");
+const colorBtn = document.getElementById("colorBtn");
+const colorPicker = document.getElementById("colorPicker");
+const classicBtn = document.getElementById("classicBtn");
 const rainbowBtn = document.getElementById("rainbowBtn");
+const smallGridBtn = document.getElementById("16");
+const mediumGridBtn = document.getElementById("32");
+const largeGridBtn = document.getElementById("48");
+const eraseBtn = document.getElementById("eraseBtn");
 const DEFAULT_SIZE = 16;
-const DEFAULT_COLOR = "#b7b2b2";
-const DEFAULT_MODE = "black";
+const DEFAULT_COLOR = "#333333";
+const DEFAULT_MODE = "color";
 
-let currentMode = "black";
+let currentMode = "color";
+let currentColor = DEFAULT_COLOR;
+let currentSize = DEFAULT_SIZE;
 
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
@@ -15,39 +23,72 @@ document.body.onmouseup = () => (mouseDown = false);
 
 function changeMode(mode) {
   currentMode = mode;
-  activeButton();
+  activeButton(currentMode);
 }
 
-function activeButton() {}
+function setCurrentColor(color) {
+  currentColor = color;
+}
+
+function activeButton(currentMode) {
+  if (currentMode === "classic") {
+    classicBtn.classList.add("active");
+    colorBtn.classList.remove("active");
+    rainbowBtn.classList.remove("active");
+  } else if (currentMode === "color") {
+    colorBtn.classList.add("active");
+    classicBtn.classList.remove("active");
+    rainbowBtn.classList.remove("active");
+  } else if (currentMode === "rainbow") {
+    colorBtn.classList.remove("active");
+    classicBtn.classList.remove("active");
+    rainbowBtn.classList.add("active");
+  }
+}
+
+function updateSize(size) {
+  currentSize = size;
+  activeSizeButton(currentSize);
+}
+
+function activeSizeButton(currentSize) {
+  if (currentSize === 16) {
+    smallGridBtn.classList.add("active");
+    mediumGridBtn.classList.remove("active");
+    largeGridBtn.classList.remove("active");
+  } else if (currentSize === 32) {
+    smallGridBtn.classList.remove("active");
+    mediumGridBtn.classList.add("active");
+    largeGridBtn.classList.remove("active");
+  } else if (currentSize === 48) {
+    smallGridBtn.classList.remove("active");
+    mediumGridBtn.classList.remove("active");
+    largeGridBtn.classList.add("active");
+  }
+}
 
 function changecolor(e) {
   if (e.type === "mouseover" && !mouseDown) return;
-  if (currentMode === "black") {
-    e.target.style.backgroundColor = "#b7b2b2";
-  } else if (currentMode === "rainbow") {
+  if (currentMode === "rainbow") {
     const randomR = Math.floor(Math.random() * 256) + 1;
     const randomG = Math.floor(Math.random() * 256) + 1;
     const randomB = Math.floor(Math.random() * 256) + 1;
     e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+  } else if (currentMode === "classic") {
+    e.target.style.backgroundColor = "#b7b2b2";
+  } else if (currentMode === "color") {
+    e.target.style.backgroundColor = currentColor;
   }
-  // if (currentMode === "rainbow") {
-  //   const randomR = Math.floor(Math.random() * 256);
-  //   const randomG = Math.floor(Math.random() * 256);
-  //   const randomB = Math.floor(Math.random() * 256);
-  //   e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
-  // } else if (currentMode === "color") {
-  //   e.target.style.backgroundColor = currentColor;
   // } else if (currentMode === "eraser") {
   //   e.target.style.backgroundColor = "#fefefe";
   // }
 }
 
-function settingGrid(size = DEFAULT_SIZE) {
+function settingGrid(size) {
   container.style.setProperty("grid-template-columns", `repeat(${size}, auto)`);
 
   for (let i = 0; i < size * size; i++) {
     const content = document.createElement("div");
-    content.style.background = "#e6e6e6";
     content.classList.add("grid-element");
     content.addEventListener("mouseover", changecolor);
     content.addEventListener("mousedown", changecolor);
@@ -59,11 +100,54 @@ function clearGrid() {
   container.innerHTML = "";
 }
 
+function eraseGrid(size) {
+  clearGrid();
+  settingGrid(currentSize);
+}
+
+//----------------Event listeners--------------------
+//Modify grid's size
 btns.forEach(btn =>
   btn.addEventListener("click", e => {
+    // updateSize(e.target.id);
     clearGrid();
     settingGrid(e.target.id);
   })
 );
 
-settingGrid();
+//Color mode
+colorBtn.addEventListener("click", function () {
+  changeMode("color");
+});
+classicBtn.addEventListener("click", function () {
+  changeMode("classic");
+});
+rainbowBtn.addEventListener("click", function () {
+  changeMode("rainbow");
+});
+
+colorPicker.addEventListener("input", e => setCurrentColor(e.target.value));
+
+//Switch active class in size buttons
+smallGridBtn.addEventListener("click", function () {
+  smallGridBtn.classList.add("active");
+  mediumGridBtn.classList.remove("active");
+  largeGridBtn.classList.remove("active");
+});
+mediumGridBtn.addEventListener("click", function () {
+  smallGridBtn.classList.remove("active");
+  mediumGridBtn.classList.add("active");
+  largeGridBtn.classList.remove("active");
+});
+largeGridBtn.addEventListener("click", function () {
+  smallGridBtn.classList.remove("active");
+  mediumGridBtn.classList.remove("active");
+  largeGridBtn.classList.add("active");
+});
+
+//Clean Grid
+eraseBtn.addEventListener("click", eraseGrid);
+
+settingGrid(DEFAULT_SIZE);
+changeMode(DEFAULT_MODE);
+updateSize(DEFAULT_SIZE);
